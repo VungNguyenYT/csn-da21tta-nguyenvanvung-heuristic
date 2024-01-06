76c86d9e1f7675e0f2be42d60bf7ffd1a4dc9208
Nguyễn Văn Vửng <130837811+VungNguyenYT@users.noreply.github.com>
2024-01-06 13:45:31 +0700

    up code

--- BFS.py
+++ BFS.py
@@ -9,9 +9,47 @@ class node:
         print(self.name)
 
 from collections import defaultdict 
+
 data = defaultdict(list)
 data['A'] = ['B', 'C', 'D']
 data['B'] = ['E', 'F']
 data['C'] = ['G', 'H']
 data['D'] = ['I', 'J']
 data['F'] = ['K', 'L', 'M']
+data['H'] = ['N', 'O']
+
+def equal(O, G):
+    return O.name == G.name
+
+def checkInArray(tmp, open):
+    for x in open:
+        if equal(x, tmp):
+            return True
+    return False
+
+#Thuật toán BFS
+def BFS(S = node('A'), G = node('M')):
+    open = []
+    Closed = []
+    open.append(S)
+    while True:
+        if len(open) == 0:
+            print('Tìm kiếm thất bại')
+            return
+        O = open.pop(0)
+        Closed.append(O)
+        if equal(O, G) == True:
+            print('Tìm kiếm thành công')
+            return
+        for x in data [O.name]:
+            tmp = node(x)
+            tmp.par = O
+
+            ok1 = checkInArray(tmp, open)
+            ok2 = checkInArray(tmp, Closed)
+            
+            #Nếu tmp không thuộc Open(O) và Close thì đưa vào cuối Open
+            if not ok1 and not ok2:
+                open.append(tmp)
+
+ 
